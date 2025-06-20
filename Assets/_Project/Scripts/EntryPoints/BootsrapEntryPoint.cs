597b19c3271351626f6a612ba1358a6c48761cad
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2025-06-20 22:01:33 +0300

    Создан ui для сцены

---
+++ Assets/_Project/Scripts/EntryPoints/BootsrapEntryPoint.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Asteroid.Generation
+{
+    public class BootstrapEntryPoint : MonoBehaviour
+    {
+        IEnumerator Start()
+        { 
+            yield return null;
+        }
+    }
+}
